73cc280ff8cc5c37360421dafa34dd99646054a5
wounds2 <m1_zaza@mail.ru>
2017-02-21 19:14:59 +0300

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,7 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {157059515},
+    sudo_users = {112235430},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
